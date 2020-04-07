22d355b308600cf25b31d8c10c27180a49584824
Sirojiddin Saidmurodov <siroj.serj15@outlook.com>
2020-04-07 19:52:28 +0300

    Done with code of Roberts algo

--- ThirdLesson/sphere.py
+++ ThirdLesson/sphere.py
@@ -10,7 +10,7 @@ class Point3D:
         self.axes = coordinates
 
     def __eq__(self, other):
-        if (self.axes == other.axes).sum() == 4:
+        if self.x == other.x and self.y == other.y and self.z == other.z:
             return True
         return False
 
@@ -31,7 +31,7 @@ class Line:
         self.visibility += 1
 
     def __eq__(self, other):
-        if self.p1 == other.p2 and self.p2 == other.p2 or self.p1 == other.p2 and self.p2 == self.p1:
+        if (self.p1 == other.p2 and self.p2 == other.p2) or (self.p1 == other.p2 and self.p2 == self.p1):
             return True
         return False
 
@@ -67,6 +67,42 @@ class Sphere:
             points_matrix[:, i, :] = points_matrix[:, i, :] @ rot_matrix
         return points_matrix
 
+    def roberts(self, longitude_count, latitude_count, points, lines, point_of_view):
+        for i in range(longitude_count - 1):
+            for j in range(1, latitude_count - 1):
+                if i == 0:
+                    temp = [points[j - 1, i, :],
+                            points[j, i + 1, :],
+                            points[j + 1, i, :]]
+                else:
+                    temp = [points[j, i - 1, :],
+                            points[j - 1, i, :],
+                            points[j, i + 1, :],
+                            points[j + 1, i, :],
+                            points[j, i - 1, :]]
+                for surface in range(len(temp) - 1):
+                    v1 = temp[surface] - points[j, i, :]
+                    v2 = temp[surface + 1] - points[j, i, :]
+                    A = v1[1] * v2[2] - v2[1] * v1[2]
+                    B = v1[2] * v2[0] - v2[2] * v1[0]
+                    C = v1[0] * v2[1] - v2[0] * v1[1]
+                    D = -(A * v1[0] + B * v1[1] + C * v1[2])
+                    #  if (A·W.X + B·W.Y + C·W.Z + D)<0 : but W is (0,0,0)
+                    if D < 0:
+                        m = 1
+                    else:
+                        m = -1
+                    #  correcting surface normal vector
+                    A, B, C, D = A * m, B * m, C * m, D * m
+                    p = point_of_view
+                    if A * p.x + B * p.y + C * p.z + D > 0:
+                        for s in range(len(lines)):
+                            if lines[s] == Line(Point3D(temp[surface]), Point3D(points[j, i, :])) or lines[s] == Line(
+                                    Point3D(temp[surface + 1]), Point3D(points[j, i, :])):
+                                lines[s] = lines[s].set_v()
+                        #  line is not visible from one side
+        return lines
+
     def get_points(self, longitude_count: int, latitude_count: int) -> list:
         u, v = np.mgrid[0:2 * np.pi:complex(longitude_count), 0:np.pi:complex(latitude_count)]
         x = np.cos(u) * np.sin(v) * self.radius
@@ -74,45 +110,17 @@ class Sphere:
         z = np.cos(v) * self.radius
         points = np.stack((x, y, z, np.ones(x.shape)))
         #  rotating points, transposing points matrix for correct matrix product
-        self.rotate(points.T, self.angle.x, self.angle.y, self.angle.z)
+        points = points.T
+        self.rotate(points, self.angle.x, self.angle.y, self.angle.z)
         #  filling lines list
         lines = []
         for i in range(longitude_count - 1):
             for j in range(latitude_count - 1):
                 #  All lines are visible by default
-                lines.append(Line(Point3D(points[:, i, j]),
-                                  Point3D(points[:, i + 1, j])))
-                lines.append(Line(Point3D(points[:, i, j]),
-                                  Point3D(points[:, i, j + 1])))
+                lines.append(Line(Point3D(points[j, i, :]),
+                                  Point3D(points[j, i + 1, :])))
+                lines.append(Line(Point3D(points[j, i, :]),
+                                  Point3D(points[j + 1, i, :])))
 
         #  delete invisible lines using Roberts algorithm
-        for i in range(longitude_count - 1):
-            for j in range(1, latitude_count - 1):
-                if i == 0:
-                    temp = [points[:, i, j - 1],
-                            points[:, i + 1, j],
-                            points[:, i, j + 1]]
-                else:
-                    temp = [points[:, i - 1, j],
-                            points[:, i, j - 1],
-                            points[:, i + 1, j],
-                            points[:, i, j + 1],
-                            points[:, i - 1, j]]
-                for surface in range(len(temp) - 1):
-                    v1 = temp[surface] - points[:, i, j]
-                    v2 = temp[surface + 1] - points[:, i, j]
-                    A = v1[1] * v2[2] - v2[1] * v1[2]
-                    B = v1[2] * v2[0] - v2[2] * v1[0]
-                    C = v1[0] * v2[1] - v2[0] * v1[1]
-                    D = -(A * v1[0] + B * v1[1] + C * v1[2])
-                    #  if (A·W.X + B·W.Y + C·W.Z + D)<0 : but W is (0,0,0)
-                    if D < 0:
-                        m = -1
-                    else:
-                        m = 1
-                    #  correcting surface normal vector
-                    A, B, C, D = A * m, B * m, C * m, D * m
-
-
-
-        return lines
+        return self.roberts(longitude_count, latitude_count, points, lines, Point3D(np.array([0, 0, -1, 1])))
--- ThirdLesson/sphereDrawer.py
+++ ThirdLesson/sphereDrawer.py
@@ -7,8 +7,9 @@ lines = sphere.get_points(18, 18)
 image = np.zeros((500, 500, 3), dtype="uint8")
 image.fill(255)
 for line in lines:
-    a, b, _ = line.to_tuple()
-    cv2.line(image, (int(a.x) + 250, int(a.y) + 250), (int(b.x) + 250, int(b.y) + 250), (0, 0, 0))
+    if line.visibility == 0:
+        a, b, _ = line.to_tuple()
+        cv2.line(image, (int(a.x) + 250, int(a.y) + 250), (int(b.x) + 250, int(b.y) + 250), (0, 0, 0))
 
 cv2.imshow("im", image)
 cv2.waitKey(0)
