03f86071a5a9bf8d4ae100dcbe2ac56663d354e0
Sirojiddin Saidmurodov <siroj.serj15@outlook.com>
2020-04-08 17:44:40 +0300

    Done with roberts algorithm

--- ThirdLesson/sphereDrawer.py
+++ ThirdLesson/sphereDrawer.py
@@ -2,14 +2,15 @@ import ThirdLesson.sphere as sp
 import cv2
 import numpy as np
 
-sphere = sp.Sphere(sp.Point3D(np.array([250, 250, 0, 1])), 200, sp.Point3D(np.array([35, 35, 15, 1])))
-lines = sphere.get_points(18, 18)
+sphere = sp.Sphere(np.array([250, 250, 250, 1]), 200, np.array([35, 35, 15, 1]))
+lines = sphere.get_points(20, 20)
+print(len(lines))
 image = np.zeros((500, 500, 3), dtype="uint8")
 image.fill(255)
-for line in lines:
-    if line.visibility == 0:
-        a, b, _ = line.to_tuple()
-        cv2.line(image, (int(a.x) + 250, int(a.y) + 250), (int(b.x) + 250, int(b.y) + 250), (0, 0, 0))
+
+for i in range(len(lines)):
+        a, b = lines[i]
+        cv2.line(image, (int(a[0]) + 250, int(a[1]) + 250), (int(b[0]) + 250, int(b[1]) + 250), (0, 0, 0))
 
 cv2.imshow("im", image)
 cv2.waitKey(0)
