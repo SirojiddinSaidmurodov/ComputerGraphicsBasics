08fb9637d8b92e085d03a7ecbed3ab39936f7050
Sirojiddin Saidmurodov <siroj.serj15@outlook.com>
2020-06-05 20:28:15 +0300

    Added virus.py

--- Virus/virus.py
+++ Virus/virus.py
@@ -9,8 +9,8 @@ from PIL import ImageTk
 # color = np.zeros((1, 1, 3), dtype='uint8')
 # color[0, 0, 1] = 255
 
-x = 60
-y = 100
+x = 40
+y = 25
 
 ill_people = 0
 people = []
@@ -41,8 +41,9 @@ def start():
     if ill_people <= 0 or ill_people >= (x * y):
         ill_people = 1
         infect()
-    infect_all(ill_people)
-    ill_people *= 2
+    else:
+        infect_all(ill_people)
+        ill_people *= 2
 
     pic = image
     pic = cv2.cvtColor(pic, cv2.COLOR_BGR2RGB)
@@ -51,9 +52,19 @@ def start():
     panel.configure(image=pic)
     panel.image = pic
 
+    healthy.set("Health people: " + str(len(people)))
+    ill.set("Ill people: " + str(1000 - len(people)))
+
 
 root = Tk()
 root.title("CoViD-19")
+root.geometry("450x400")
+root.resizable(0, 0)
+
+healthy = StringVar(root)
+ill = StringVar(root)
+healthy.set("Health people: " + str(len(people)))
+ill.set("Ill people: " + str(1000 - len(people)))
 
 im = image
 im = cv2.cvtColor(im, cv2.COLOR_BGR2RGB)
@@ -61,8 +72,11 @@ im = Image.fromarray(im)
 im = ImageTk.PhotoImage(im)
 panel = Label(image=im)
 panel.image = im
-panel.pack(side='top')
+panel.place(relx=0, rely=0)
 
-btn = Button(text="update", command=start)
-btn.pack(side="bottom")
+btn = Button(text="Next day", command=start)
+btn.place(relx=0.8, rely=0.9)
+Label(text="People: 1000").place(relx=0.7, rely=0.05)
+Label(textvariable=healthy).place(relx=0.7, rely=0.15)
+Label(textvariable=ill).place(relx=0.7, rely=0.25)
 root.mainloop()
